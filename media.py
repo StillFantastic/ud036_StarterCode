0f110841d499d7f9755ee4598ce7ecce323b4de4
StillFantasy <bl33234679@gmail.com>
2018-06-19 08:30:06 +0800

    change the indentation from 2 spaces to 4 spaces

--- media.py
+++ media.py
@@ -1,8 +1,6 @@
 # Class that stores information of movies
 class Movie():
-  def __init__(self, title, poster_image_url, trailer_youtube_url):
-    self.title = title
-    self.poster_image_url = poster_image_url
-    self.trailer_youtube_url = trailer_youtube_url
-
-
+    def __init__(self, title, poster_image_url, trailer_youtube_url):
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
